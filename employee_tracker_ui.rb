0e331b610955cb29bd4244870538d45b333d698e
Dustin Lewis & Justin Pacubas <student@epicodus.com>
2014-08-18 13:54:28 -0700

    Add spec helper, division class, division spec and update UI to add divisions.

--- employee_tracker_ui.rb
+++ employee_tracker_ui.rb
@@ -1,5 +1,6 @@
 require 'active_record'
 require './lib/employee'
+require './lib/division'
 require 'pry'
 
 database_configuration = YAML::load(File.open('./db/config.yml'))
@@ -7,11 +8,29 @@ development_configuration = database_configuration["development"]
 ActiveRecord::Base.establish_connection(development_configuration)
 
 def welcome
-  puts "Welcome to the Employee Tracker!"
-  menu
+  user_choice = nil
+  until user_choice == 'x'
+
+    puts "\n***Welcome to the Employee Tracker!***\n"
+    puts "Press '1' to go to the employee menu"
+    puts "Press '2' to go to the divisions menu"
+    puts "Press 'x' to exit"
+
+    user_choice = gets.chomp
+    case user_choice
+    when '1'
+      employee_menu
+    when '2'
+      divisions_menu
+    when 'x'
+      puts "Goodbye."
+    else
+      puts "Sorry, not a valid entry."
+    end
+  end
 end
 
-def menu
+def employee_menu
   choice = nil
   until choice == 'x'
     puts "Press 'e' to add an employee, 'l' to list your employees."
@@ -19,9 +38,9 @@ def menu
     choice = gets.chomp
     case choice
     when 'e'
-      add
+      add_employee
     when 'l'
-      list
+      list_employees
     when 'x'
       puts "Good-bye!"
     else
@@ -30,18 +49,52 @@ def menu
   end
 end
 
-def add
+def add_employee
   puts "Type in the new employee name:"
   employee_name = gets.chomp
   employee = Employee.new({:name => employee_name})
   employee.save
-  puts "'#{employee.name}' has been added to your To Do list."
+  puts "'#{employee.name}' has been added to your employee list."
 end
 
-def list
+def list_employees
   puts "Here are all your employees:"
   employees = Employee.all
   employees.each { |employee| puts employee.name }
+end
+
+def divisions_menu
+  choice = nil
+  until choice == 'x'
+    puts "Press 'a' to add a division"
+    puts "Press 'l' to list your divisions"
+    puts "Press 'x' to exit"
+    choice = gets.chomp
+    case choice
+    when 'a'
+      add_division
+    when 'l'
+      list_divisions
+    when 'x'
+      puts "Good-bye!"
+    else
+      puts "Sorry, that wasn't a valid option."
+    end
+  end
+end
+
+def add_division
+  puts "Type in the new division name:"
+  division_name = gets.chomp
+  division = Division.new({:name => division_name})
+  division.save
+  puts "'#{division.name}' has been added to your division list."
+end
+
+def list_divisions
+  puts "Here are all your divisions:"
+  divisions = Division.all
+  divisions.each { |division| puts division.name }
 end
 
 welcome
